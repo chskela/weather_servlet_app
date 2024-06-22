7c1bf4bbb1feb7704cc15345f48b5c5a2f28b9d9
Chistyakov Alexey <chskela@gmail.com>
2024-06-22 14:28:27 +0300

    feat: create entity Location

---
+++ src/main/kotlin/models/entities/Location.kt
@@ -0,0 +1,26 @@
+package models.entities
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name="locations")
+class Location (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id:Long? = null,
+
+    @Column(name="name")
+    val name:String,
+
+    @ManyToOne
+    @JoinColumn(name  = "user_id", foreignKey = ForeignKey(name  = "fk_locations_users"))
+    val user: User,
+
+    @Column(name="latitude")
+    val latitude:Double,
+
+    @Column(name="longitude")
+    val longitude:Double,
+)
+
+
--- src/main/kotlin/models/entities/User.kt
+++ src/main/kotlin/models/entities/User.kt
@@ -3,16 +3,16 @@ package models.entities
 import jakarta.persistence.*
 
 @Entity
-@Table(name="users")
+@Table(name="users", indexes = [Index(name = "idx_login", columnList = "login", unique = true)])
 class User (
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name="id")
     val id: Long? = null,
 
-    @Column(name="login")
-    val login: String = "",
+    @Column(name="login", unique = true)
+    val login: String,
 
     @Column(name="login")
-    val password: String = ""
+    val password: String
 )
