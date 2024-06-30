af5a643706c51c4a61fcaf1e8aedccbeae3b6d41
Chistyakov Alexey <chskela@gmail.com>
2024-06-30 22:13:11 +0300

    feat: create object with constants

---
+++ src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,13 @@
+package utils
+
+object Constants {
+    const val SIGN_UP = "sign-up"
+    const val EMAIL = "email"
+    const val PASSWORD = "password"
+    const val PASSWORD_CONFIRMATION = "passwordConfirmation"
+    const val ERROR = "error"
+    const val WEATHERS = "weathers"
+    const val LOGIN = "login"
+    const val INDEX = "index"
+    const val SESSION_ID = "sessionId"
+}
