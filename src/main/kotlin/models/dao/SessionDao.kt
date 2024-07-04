720cf04c0614af065995ae464dea5694f987049c
Chistyakov Alexey <chskela@gmail.com>
2024-07-05 00:21:58 +0300

    fix: fixed HomeServlet and SessionDao
    Fixed exception to SessionNotFoundException

--- src/main/kotlin/models/dao/SessionDao.kt
+++ src/main/kotlin/models/dao/SessionDao.kt
@@ -9,7 +9,19 @@ class SessionDao(
     private val entityManager: EntityManager = PersistenceUtil.entityManagerFactory.createEntityManager()
 ) {
     fun findSessionById(uuid: UUID): Result<Session> {
-        return runCatching { entityManager.find(Session::class.java, uuid) }
+        val txn = entityManager.transaction
+
+        return try {
+            val session = entityManager.find(Session::class.java, uuid)
+
+            if (session != null) {
+                Result.success(session)
+            } else Result.failure(SessionNotFoundException("Session not found"))
+
+        } catch (e: Exception) {
+            txn.rollback()
+            Result.failure(SessionNotFoundException("Session not found"))
+        }
     }
 
     fun insert(session: Session): Result<Session> {
