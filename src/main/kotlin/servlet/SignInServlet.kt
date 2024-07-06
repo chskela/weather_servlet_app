c7849d64d13b89005f1ba085db8bc21e144a2c92
Chistyakov Alexey <chskela@gmail.com>
2024-07-06 21:13:34 +0300

    feat: create SignIn Servlet
    Create SignInServlet - get and post request; and create sign-in page.
    
    Closes #27.

---
+++ src/main/kotlin/servlet/SignInServlet.kt
@@ -0,0 +1,68 @@
+package servlet
+
+import exception.UserNotExistsException
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import models.dao.SessionDao
+import models.dao.UserDao
+import models.entities.Session
+import utils.Constants.EMAIL
+import utils.Constants.EMAIL_CANNOT_BE_BLANK
+import utils.Constants.ERROR
+import utils.Constants.PASSWORD
+import utils.Constants.PASSWORD_CANNOT_BE_BLANK
+import utils.Constants.PASSWORD_DOES_NOT_MATCH
+import utils.Constants.SESSION_ID
+import utils.Constants.SIGN_IN
+import utils.Constants.USER_DOES_NOT_EXIST
+import utils.md5
+import java.time.LocalDateTime
+
+@WebServlet(name = "SignInServlet", urlPatterns = ["/sign-in"])
+class SignInServlet(
+    private val userDao: UserDao = UserDao(),
+    private val sessionDao: SessionDao = SessionDao(),
+) : BaseServlet() {
+
+    override fun doGet(request: HttpServletRequest, response: HttpServletResponse) {
+        templateEngine.process(SIGN_IN, context, response.writer)
+    }
+
+    override fun doPost(request: HttpServletRequest, response: HttpServletResponse) {
+        val email = request.getParameter(EMAIL)
+        val password = request.getParameter(PASSWORD)
+
+        if (email.isNullOrBlank()) {
+            context.setVariable(ERROR, EMAIL_CANNOT_BE_BLANK)
+            templateEngine.process(SIGN_IN, context, response.writer)
+        } else if (password.isNullOrBlank()) {
+            context.setVariable(ERROR, PASSWORD_CANNOT_BE_BLANK)
+            context.setVariable(EMAIL, email)
+            templateEngine.process(SIGN_IN, context, response.writer)
+        } else {
+            val user = userDao.findUserByLogin(email)
+                .onFailure { e ->
+                    if (e is UserNotExistsException) {
+                        context.setVariable(ERROR, USER_DOES_NOT_EXIST)
+                        context.setVariable(EMAIL, email)
+                        templateEngine.process(SIGN_IN, context, response.writer)
+                    } else throw e
+                }.getOrThrow()
+
+            if (password.md5() != user.password) {
+                context.setVariable(ERROR, PASSWORD_DOES_NOT_MATCH)
+                context.setVariable(EMAIL, email)
+                templateEngine.process(SIGN_IN, context, response.writer)
+            } else {
+                val session = sessionDao
+                    .insert(Session(user = user, expiresAt = LocalDateTime.now().withHour(1)))
+                    .getOrThrow()
+
+                response.addCookie(Cookie(SESSION_ID, session.id.toString()))
+                response.sendRedirect(request.contextPath + "/")
+            }
+        }
+    }
+}
--- src/main/kotlin/utils/Constants.kt
+++ src/main/kotlin/utils/Constants.kt
@@ -2,6 +2,7 @@ package utils
 
 object Constants {
     const val SIGN_UP = "sign-up"
+    const val SIGN_IN = "sign-in"
     const val EMAIL = "email"
     const val PASSWORD = "password"
     const val PASSWORD_CONFIRMATION = "passwordConfirmation"
@@ -10,4 +11,8 @@ object Constants {
     const val LOGIN = "login"
     const val INDEX = "index"
     const val SESSION_ID = "sessionId"
+    const val EMAIL_CANNOT_BE_BLANK= "Email cannot be blank"
+    const val PASSWORD_CANNOT_BE_BLANK= "Password cannot be blank"
+    const val USER_DOES_NOT_EXIST= "User does not exist"
+    const val PASSWORD_DOES_NOT_MATCH= "Password does not match"
 }
