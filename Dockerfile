0dfd5655aa821d855585a44b14130c5f1171063f
Chistyakov Alexey <chskela@gmail.com>
2024-09-06 16:53:09 +0300

    feat: create SessionDaoTest
    Closed #50.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM gradle:8.6.0-jdk17-alpine AS build
-COPY --chown=gradle:gradle . /home/gradle/src
-WORKDIR /home/gradle/src
-RUN gradle war --no-daemon
+#FROM gradle:8.6.0-jdk17-alpine AS build
+#COPY --chown=gradle:gradle . /home/gradle/src
+#WORKDIR /home/gradle/src
+#RUN gradle war --no-daemon
 
 FROM tomcat:jre21-temurin-jammy
 LABEL authors="chskela"
@@ -10,8 +10,8 @@ LABEL authors="chskela"
 RUN rm -fr "$CATALINA_HOME"/webapps/*
 
 # We copy our WAR application inside tomcat webapps
-COPY --from=build /home/gradle/src/build/libs/weather_servlet_app-1.0.war $CATALINA_HOME/webapps/
-#COPY ./build/libs/tennis_match_scoreboard-1.0.war $CATALINA_HOME/webapps/
+#COPY --from=build /home/gradle/src/build/libs/weather_servlet_app-1.0.war $CATALINA_HOME/webapps/
+COPY ./build/libs/weather_servlet_app-1.0.war $CATALINA_HOME/webapps/
 
 COPY ./server.xml /usr/local/tomcat/conf
 
---
+++ src/test/kotlin/models/dao/SessionDaoTest.kt
@@ -0,0 +1,88 @@
+package models.dao
+
+import exception.SessionNotFoundException
+import jakarta.persistence.EntityManager
+import models.database.TestContainerConfig
+import models.entities.Session
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import services.dto.AuthorizationDTO
+import services.dto.toUser
+import java.time.LocalDateTime
+import java.util.*
+
+class SessionDaoTest{
+    val entityManager: EntityManager = TestContainerConfig.entityManagerFactory.createEntityManager()
+    private val userDao = UserDao(entityManager)
+    private val sessionDao = SessionDao(entityManager)
+
+    private val candidate: AuthorizationDTO = AuthorizationDTO("test@gmail.com", "password")
+
+    @BeforeEach
+    fun setup() {
+        val txn = entityManager.transaction
+        try {
+            txn.begin()
+            entityManager.createQuery("DELETE FROM Session").executeUpdate()
+            entityManager.createQuery("DELETE FROM Location").executeUpdate()
+            entityManager.createQuery("DELETE FROM User").executeUpdate()
+            txn.commit()
+        } catch (e: Exception) {
+            txn.rollback()
+        }
+    }
+
+    @Test
+    fun `insert new Session should returned added Session`(){
+        //when
+        val addedUser = userDao.insert(candidate.toUser()).getOrThrow()
+        val session = Session(user = addedUser, expiresAt = LocalDateTime.now().withHour(1))
+        val addedSession = sessionDao.insert(session).getOrThrow()
+
+        //then
+        assertNotNull(addedSession.id)
+        assertNotNull(addedSession.user)
+        assertNotNull(addedSession.expiresAt)
+        assertEquals(addedSession.user.login, candidate.email)
+    }
+
+
+    @Test
+    fun `remove existing Session by UUID should returned removed 1`(){
+        //when
+        val addedUser = userDao.insert(candidate.toUser()).getOrThrow()
+        val session = Session(user = addedUser, expiresAt = LocalDateTime.now().withHour(1))
+        val addedSession = sessionDao.insert(session).getOrThrow()
+        val result = sessionDao.removeSessionById(addedSession.id).getOrThrow()
+
+        //then
+        assertEquals(1, result)
+    }
+
+
+    @Test
+    fun `find existing Session by UUID should returned Session`(){
+        //when
+        val addedUser = userDao.insert(candidate.toUser()).getOrThrow()
+        val session = Session(user = addedUser, expiresAt = LocalDateTime.now().withHour(1))
+        val addedSession = sessionDao.insert(session).getOrThrow()
+        val result = sessionDao.findSessionById(addedSession.id).getOrThrow()
+
+        //then
+        assertNotNull(result.id)
+        assertNotNull(result.user)
+        assertNotNull(result.expiresAt)
+        assertEquals(result.user.login, candidate.email)
+    }
+
+    @Test
+    fun `find not existing Session by UUID should thrown SessionNotFoundException`(){
+        //when
+        val fakeUuid = UUID.randomUUID()
+
+        //then
+        assertThrows<SessionNotFoundException> { sessionDao.findSessionById(fakeUuid).getOrThrow() }
+    }
+}
