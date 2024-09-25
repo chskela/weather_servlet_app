cc3005720da1a1e27704c2a010b3fac199c04dd3
Chistyakov Alexey <chskela@gmail.com>
2024-09-25 21:03:38 +0300

    fix: fixed period

--- src/main/kotlin/services/SessionService.kt
+++ src/main/kotlin/services/SessionService.kt
@@ -9,11 +9,12 @@ class SessionService {
     fun removeSessionExpiredAtTime(date: Date) {
         val timer = Timer()
         val task = Task()
-        val period = 1000L * 60 * 3
+        val period = 1000L * 60 * 60 * 24
 
         timer.schedule(task, date, period)
     }
-    private class Task(private val sessionDao: SessionDao = SessionDao()) : TimerTask(){
+
+    private class Task(private val sessionDao: SessionDao = SessionDao()) : TimerTask() {
         override fun run() {
             sessionDao.removeSessionExpiredAtTime(LocalDateTime.now())
         }
